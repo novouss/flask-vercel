e77868ffc66b2aa7041caddddbaa74c9e284fb91
Raymond Gorospe <raymondgorospe1113@gmail.com>
2025-01-30 23:24:36 +0800

    Reapply "fix(bug) missed a / in app.route"
    This reverts commit 2cbd207a9a0f9626e88f72be2d598b08a6f06994.

--- app.py
+++ app.py
@@ -7,13 +7,13 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
-@app.route('/api/', methods=['GET'])
+@app.route('/api', methods=['GET'])
 def data():
     names = request.args.getlist('name')
     data = get_data()
     students = { entry['name']: entry['marks'] for entry in data if entry['name'] in names }
     results = [ value for _, value in students.items() ]
-    return { "marks": results }
+    return jsonify(results)
 
 if __name__ == '__main__':
     app.run(debug=True)
