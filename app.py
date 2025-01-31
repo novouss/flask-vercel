faa2fa8908c9aa240ad31d76e01c6a6fdfcff362
Raymond Gorospe <raymondgorospe1113@gmail.com>
2025-01-31 18:55:40 +0800

    feat(api) utilized pandas approach

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request, jsonify
 from flask_cors import CORS
-from api.index import get_data
+# from api.index import get_data
+import pandas as pd
 
 app = Flask(__name__)
 cors = CORS(app)
@@ -12,9 +13,17 @@ def home():
 @app.route('/api', methods=['GET'])
 def data():
     names = request.args.getlist('name')
-    data = get_data()
-    students = { entry['name']: entry['marks'] for entry in data if entry['name'] in names }
-    results = [ value for _, value in students.items() ]
+    with open('student_data.json', 'r') as file:
+        data = pd.read_json(file)
+    # data = get_data()
+    # students = { entry['name']: entry['marks'] for entry in data if entry['name'] in names }
+    results = []
+    for name in names:
+        mark = data[data['name'] == name]
+        if not mark.empty:
+            results.append(int(mark['marks'].values[0]))
+    
+    # results = [ value for _, value in students.items() ]
     return jsonify({ "marks": results })
 
 if __name__ == '__main__':
